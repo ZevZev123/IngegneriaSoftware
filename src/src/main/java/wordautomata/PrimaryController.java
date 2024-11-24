d568f80d93f89d0c3906f7aa6c3da4f39eb685a3
ZevZev <davidzahariev@gmail.com>
2024-11-24 20:56:42 +0100

    Aggiornamento fxml e creazione funzione initialize()
    Aggiunto ScrollPane per NodeEdgeList e possibilità di ingrandire orizontalmente con il mouse.
    Inserita immagine di RUN.
    Creata Label di Status (funzione ancora da creare)

--- src/src/main/java/wordautomata/PrimaryController.java
+++ src/src/main/java/wordautomata/PrimaryController.java
@@ -1,12 +1,26 @@
 package wordautomata;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
 public class PrimaryController {
+    @FXML private VBox nodeEdgeList;
+    @FXML private Label statusLabel;
 
+    @FXML
+    private void initialize(){
+        Label label;
+        for (int i = 1; i <= 50; i++) {
+            label = new Label("Label " + i); // Crea una nuova Label
+            label.setTextAlignment(TextAlignment.LEFT); // Assicura che il testo sia allineato a sinistra
+            nodeEdgeList.getChildren().add(label); // Aggiungi la Label alla VBox
+        }
+    }
 
     /*
+    // in caso di aggiunta seconda pagina:
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
