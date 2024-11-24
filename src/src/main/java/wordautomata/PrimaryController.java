8b8cd88f10c317c3669fda06726f05df3565fb47
ZevZev <davidzahariev@gmail.com>
2024-11-24 23:47:56 +0100

    Sistemazioni generali
    Aggiunta gif con funzione per cambiare la classe stile.
    Sistemazione generale dei colori.
    Aggiunto tooltip alla Label history visualiser

--- src/src/main/java/wordautomata/PrimaryController.java
+++ src/src/main/java/wordautomata/PrimaryController.java
@@ -1,7 +1,9 @@
 package wordautomata;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
@@ -9,14 +11,35 @@ public class PrimaryController {
     @FXML private VBox nodeEdgeList;
     @FXML private Label statusLabel;
 
+    @FXML private Button runButton;
+    @FXML private Label history;
+
     @FXML
     private void initialize(){
+        updateToolTip();
+
         Label label;
         for (int i = 1; i <= 50; i++) {
             label = new Label("Label " + i); // Crea una nuova Label
             label.setTextAlignment(TextAlignment.LEFT); // Assicura che il testo sia allineato a sinistra
             nodeEdgeList.getChildren().add(label); // Aggiungi la Label alla VBox
         }
+    }
+
+    @FXML
+    private void changeIcon(){
+        System.out.println(runButton.getStyleClass());
+        if (runButton.getStyleClass().contains("RunButton")) {
+            runButton.getStyleClass().setAll("button", "loadingButton"); 
+        } else {
+            runButton.getStyleClass().setAll("button", "RunButton");
+        }
+    }
+
+    private void updateToolTip(){
+        Tooltip tooltip = new Tooltip(history.getText());
+
+        history.setTooltip(tooltip);
     }
 
     /*
