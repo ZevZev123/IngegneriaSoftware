43c3eb4dc0bf77bdd9d20aea8a1ad10291302d71
ZevZev <davidzahariev@gmail.com>
2024-12-10 17:56:51 +0100

    Inizio fusione

--- src/src/main/java/module-info.java
+++ src/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module wordautomata {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
     opens wordautomata to javafx.fxml;
     exports wordautomata;
--- src/src/main/java/wordautomata/PrimaryController.java
+++ src/src/main/java/wordautomata/PrimaryController.java
@@ -1,5 +1,8 @@
 package wordautomata;
 
+import wordautomata.graph.*;
+import wordautomata.graphview.*;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -24,6 +27,83 @@ public class PrimaryController {
             label.setTextAlignment(TextAlignment.LEFT); // Assicura che il testo sia allineato a sinistra
             nodeEdgeList.getChildren().add(label); // Aggiungi la Label alla VBox
         }
+
+        // -------------------------------
+        // CODICE PER VISUALIZZARE I GRAFI
+        // -------------------------------
+        Graph<String, String> g = build_flower_graph();
+        //Graph<String, String> g = build_flower_graph();
+        System.out.println(g);
+        
+        SmartPlacementStrategy initialPlacement = new SmartCircularSortedPlacementStrategy();
+        //SmartPlacementStrategy initialPlacement = new SmartRandomPlacementStrategy();
+
+        ForceDirectedLayoutStrategy<String> automaticPlacementStrategy = new ForceDirectedSpringGravityLayoutStrategy<>();
+        //ForceDirectedLayoutStrategy<String> automaticPlacementStrategy = new ForceDirectedSpringSystemLayoutStrategy<>();
+
+        SmartGraphPanel<String, String> graphView = new SmartGraphPanel<>(g, initialPlacement, automaticPlacementStrategy);
+
+        /*
+        After creating, you can change the styling of some element.
+        This can be done at any time afterwards.
+        */
+        if (g.numVertices() > 0) {
+            graphView.getStylableVertex("A").setStyleInline("-fx-fill: gold; -fx-stroke: brown;");
+        }
+
+        /*
+        IMPORTANT: Must call init() after scene is displayed, so we can have width and height values
+        to initially place the vertices according to the placement strategy.
+        */
+        graphView.init();
+
+        /*
+        Bellow you can see how to attach actions for when vertices and edges are double-clicked
+         */        
+        graphView.setVertexDoubleClickAction((SmartGraphVertex<String> graphVertex) -> {
+            System.out.println("Vertex contains element: " + graphVertex.getUnderlyingVertex().element());
+                      
+            //toggle different styling
+            if( !graphVertex.removeStyleClass("myVertex") ) {
+                /* for the golden vertex, this is necessary to clear the inline
+                css class. Otherwise, it has priority. Test and uncomment. */
+                //graphVertex.setStyle(null);
+                
+                graphVertex.addStyleClass("myVertex");
+            }
+            
+            //want fun? uncomment below with automatic layout
+            //g.removeVertex(graphVertex.getUnderlyingVertex());
+            //graphView.update();
+        });
+
+        graphView.setEdgeDoubleClickAction(graphEdge -> {
+            System.out.println("Edge contains element: " + graphEdge.getUnderlyingEdge().element());
+            //dynamically change the style when clicked; style propagated to the arrows
+            graphEdge.setStyleClass("myEdge");
+
+            // can apply different styling to the arrows programmatically.
+            // graphEdge.getStylableArrow().setStyleClass("arrow");
+            
+            //uncomment to see edges being removed after click
+            //Edge<String, String> underlyingEdge = graphEdge.getUnderlyingEdge();
+            //g.removeEdge(underlyingEdge);
+            //graphView.update();
+        });
+
+        /*
+        Should proceed with automatic layout or keep original placement?
+        If using SmartGraphDemoContainer you can toggle this in the UI 
+         */
+        //graphView.setAutomaticLayout(true);
+
+        /* 
+        Uncomment lines to test adding of new elements
+         */
+        //continuously_test_adding_elements(g, graphView);
+        //stage.setOnCloseRequest(event -> {
+        //    running = false;
+        //});
     }
 
     @FXML
@@ -49,4 +129,45 @@ public class PrimaryController {
         App.setRoot("secondary");
     }
     */
+
+    private Graph<String, String> build_flower_graph() {
+
+        Graph<String, String> g = new GraphEdgeList<>();
+
+        g.insertVertex("A");
+        g.insertVertex("B");
+        g.insertVertex("C");
+        g.insertVertex("D");
+        g.insertVertex("E");
+        g.insertVertex("F");
+        g.insertVertex("G");
+
+        g.insertEdge("A", "B", "1");
+        g.insertEdge("A", "C", "2");
+        g.insertEdge("A", "D", "3");
+        g.insertEdge("A", "E", "4");
+        g.insertEdge("A", "F", "5");
+        g.insertEdge("A", "G", "6");
+
+        g.insertVertex("H");
+        g.insertVertex("I");
+        g.insertVertex("J");
+        g.insertVertex("K");
+        g.insertVertex("L");
+        g.insertVertex("M");
+        g.insertVertex("N");
+
+        g.insertEdge("H", "I", "7");
+        g.insertEdge("H", "J", "8");
+        g.insertEdge("H", "K", "9");
+        g.insertEdge("H", "L", "10");
+        g.insertEdge("H", "M", "11");
+        g.insertEdge("H", "N", "12");
+
+        g.insertEdge("A", "H", "0");
+
+        //g.insertVertex("ISOLATED");
+        
+        return g;
+    }
 }
